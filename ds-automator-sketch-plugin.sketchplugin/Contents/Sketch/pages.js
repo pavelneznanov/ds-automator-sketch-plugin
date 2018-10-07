e5166f33f80e84dece3acc37d6c03c99a72032d5
npi <npi@iMac-npi.local>
2018-10-07 23:49:26 +0300

    Доработки сортировки

--- ds-automator-sketch-plugin.sketchplugin/Contents/Sketch/pages.js
+++ ds-automator-sketch-plugin.sketchplugin/Contents/Sketch/pages.js
@@ -104,54 +104,144 @@ __webpack_require__.r(__webpack_exports__);
 function pages(context) {
   var doc = context.document;
   var pages = doc.pages();
-  pages.sort(function (a, b) {
-    var pageA = a.name();
-    var pageB = b.name();
+  var selection = context.selection;
 
-    if (pageA < pageB) {
-      return -1;
-    }
+  var UI = __webpack_require__(/*! sketch/ui */ "sketch/ui");
 
-    if (pageA > pageB) {
-      return 1;
-    }
-
-    return 0;
-  });
-  pages.forEach(function (page, i) {
+  var checkCyrillic;
+  pages.forEach(function (page) {
     var pageName = page.name();
-    var docRegex = /documentation/g;
-    var comRegex = /components|Symbols/g;
+    var cyrillicRegex = /[а-яА-Я]/g;
 
-    if (pageName.search(docRegex) != -1) {
-      var pageMemo = pages[i];
-      pageMemo.setName('1-documentation');
-      pages.splice(i, 1);
-      pages.unshift(pageMemo);
-    }
-
-    if (pageName.search(comRegex) != -1) {
-      var _pageMemo = pages[i];
-
-      _pageMemo.setName('0-components');
-
-      pages.splice(i, 1);
-      pages.unshift(_pageMemo);
+    if (pageName.search(cyrillicRegex) != -1) {
+      checkCyrillic = true;
     }
   });
-  pages.forEach(function (page, i) {
-    var pageName = page.name();
-    pageName = pageName.trim().replace(' ', '-');
-    pageName = pageName.replace(/(_+)|(-+)/g, '-');
-    pageName = pageName.replace(/-+/g, '-');
-    var numberFilter = /[\d+]\-(\w+)/;
 
-    if (pageName.search(numberFilter) == -1) {
-      page.setName(i + '-' + pageName);
-    }
-  });
-  doc.showMessage('All pages has been formatted.');
+  if (checkCyrillic) {
+    UI.alert('Внимание!', 'Названия страниц не должны содержать кириллические символы.');
+  } else {
+    pages.sort(function (a, b) {
+      var pageA = a.name();
+      var pageB = b.name(); // return a-b;
+
+      if (pageA < pageB) {
+        return -1;
+      }
+
+      if (pageA > pageB) {
+        return 1;
+      }
+
+      return 0;
+    });
+    pages.forEach(function (page, i) {
+      var pageName = page.name();
+      var docRegex = /documentation/g;
+      var comRegex = /components|Symbols/g;
+
+      if (pageName.search(docRegex) != -1) {
+        // let pageMemo = pages[i];
+        // pageMemo.setName('1-documentation');
+        // pages.splice(i, 1);
+        // pages.unshift(pageMemo);
+        var pageMemo = page;
+        pageMemo.setName('2-documentation');
+        pages.splice(i, 1);
+        pages.splice(0, 0, pageMemo);
+      }
+
+      if (pageName.search(comRegex) != -1) {
+        var _pageMemo = pages[i];
+
+        _pageMemo.setName('1-components');
+
+        pages.splice(i, 1);
+        pages.unshift(_pageMemo); // let pageMemo = page;
+        // pageMemo.setName('1-components');
+        // pages.splice(i, 1);
+        // pages.splice(0, 0, pageMemo);
+      }
+    }); // pages.sort(function(a, b) {
+    //   var pageA = a.name();
+    //   var pageB = b.name();
+    //   return a-b;
+    //   // if (pageA < pageB) {
+    //   //   return -1;
+    //   // }
+    //   // if (pageA > pageB) {
+    //   //   return 1;
+    //   // }
+    //   // return 0;
+    // });
+
+    pages.forEach(function (page, i) {
+      var pageName = page.name();
+      pageName = pageName.trim().replace(' ', '-');
+      pageName = pageName.replace(/(_+)|(-)|(\s)/g, '-');
+      pageName = pageName.replace(/-+/g, '-');
+      pageName = pageName.toLowerCase();
+      var numberFilter = /(\d+)-([a-zA-Zа-яА-Я]+|\d+)-*/g; // let numberFilter = /[\d+]\-(\w+)/;
+
+      if (pageName.search(numberFilter) == -1) {
+        page.setName(i + 1 + '-' + pageName);
+      }
+    });
+    doc.showMessage('All pages has been formatted.');
+  } // pages.sort(function(a, b) {
+  //   var pageA = a.name();
+  //   var pageB = b.name();
+  //   return a-b;
+  //   // if (pageA < pageB) {
+  //   //   return -1;
+  //   // }
+  //   // if (pageA > pageB) {
+  //   //   return 1;
+  //   // }
+  //   // return 0;
+  // });
+  // pages.forEach(function(page, i) {
+  //   let pageName = page.name();
+  //   let docRegex = /documentation/g;
+  //   let comRegex = /components|Symbols/g;
+  //   if (pageName.search(docRegex) != -1) {
+  //     let pageMemo = pages[i];
+  //     pageMemo.setName('1-documentation');
+  //     pages.splice(i, 1);
+  //     pages.unshift(pageMemo);
+  //   }
+  //   if (pageName.search(comRegex) != -1) {
+  //     let pageMemo = pages[i];
+  //     pageMemo.setName('0-components');
+  //     pages.splice(i, 1);
+  //     pages.unshift(pageMemo);
+  //   }
+  // });
+  // pages.forEach(function(page, i) {
+  //   let pageName = page.name();
+  //   pageName = pageName.trim().replace(' ', '-');
+  //   pageName = pageName.replace(/(_+)|(-+)/g, '-');
+  //   pageName = pageName.replace(/-+/g, '-');
+  //   pageName = pageName.toLowerCase();
+  //   let numberFilter = /(\d+)-([a-zA-Zа-яА-Я]+|\d+)-*/g;
+  //   if (pageName.search(numberFilter) == -1) {
+  //     page.setName(i + '-' + pageName);
+  //   }
+  // });
+  // doc.showMessage('All pages has been formatted.');
+
 }
+
+/***/ }),
+
+/***/ "sketch/ui":
+/*!****************************!*\
+  !*** external "sketch/ui" ***!
+  \****************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("sketch/ui");
 
 /***/ })
 
--- src/pages.js
+++ src/pages.js
@@ -1,47 +1,63 @@
 export function pages (context) {
   var doc = context.document;
   var pages = doc.pages();
+  var selection = context.selection;
+  var UI = require('sketch/ui');
 
-  pages.sort(function(a, b) {
-    var pageA = a.name();
-    var pageB = b.name();
-    if (pageA < pageB) {
-      return -1;
-    }
-    if (pageA > pageB) {
-      return 1;
-    }
-    return 0;
-  });
-
-  pages.forEach(function(page, i) {
+  let checkCyrillic;
+  pages.forEach(function(page) {
     let pageName = page.name();
-    let docRegex = /documentation/g;
-    let comRegex = /components|Symbols/g;
-    if (pageName.search(docRegex) != -1) {
-      let pageMemo = pages[i];
-      pageMemo.setName('1-documentation');
-      pages.splice(i, 1);
-      pages.unshift(pageMemo);
-    }
-    if (pageName.search(comRegex) != -1) {
-      let pageMemo = pages[i];
-      pageMemo.setName('0-components');
-      pages.splice(i, 1);
-      pages.unshift(pageMemo);
+    let cyrillicRegex = /[а-яА-Я]/g;
+    if (pageName.search(cyrillicRegex) != -1) {
+      checkCyrillic = true;
     }
   });
+  if (checkCyrillic) {
+    UI.alert('Внимание!', 'Названия страниц не должны содержать кириллические символы.');
+  } else {
 
-  pages.forEach(function(page, i) {
-    let pageName = page.name();
-    pageName = pageName.trim().replace(' ', '-');
-    pageName = pageName.replace(/(_+)|(-+)/g, '-');
-    pageName = pageName.replace(/-+/g, '-');
-    let numberFilter = /[\d+]\-(\w+)/;
-    if (pageName.search(numberFilter) == -1) {
-      page.setName(i + '-' + pageName);
-    }
-  });
+    pages.sort(function(a, b) {
+      var pageA = a.name();
+      var pageB = b.name();
+      // return a-b;
+      if (pageA < pageB) {
+        return -1;
+      }
+      if (pageA > pageB) {
+        return 1;
+      }
+      return 0;
+    });
 
-  doc.showMessage('All pages has been formatted.');
+    pages.forEach(function(page, i) {
+      let pageName = page.name();
+      let docRegex = /documentation/g;
+      let comRegex = /components|Symbols/g;
+      if (pageName.search(docRegex) != -1) {
+        let pageMemo = page;
+        pageMemo.setName('2-documentation');
+        pages.splice(i, 1);
+        pages.splice(0, 0, pageMemo);
+      }
+      if (pageName.search(comRegex) != -1) {
+        let pageMemo = pages[i];
+        pageMemo.setName('1-components');
+        pages.splice(i, 1);
+        pages.unshift(pageMemo);
+      }
+    });
+    
+    pages.forEach(function(page, i) {
+      let pageName = page.name();
+      pageName = pageName.trim().replace(' ', '-');
+      pageName = pageName.replace(/(_+)|(-)|(\s)/g, '-');
+      pageName = pageName.replace(/-+/g, '-');
+      pageName = pageName.toLowerCase();
+      let numberFilter = /(\d+)-([a-zA-Zа-яА-Я]+|\d+)-*/g;
+      if (pageName.search(numberFilter) == -1) {
+        page.setName(i+1 + '-' + pageName);
+      }
+    });
+    doc.showMessage('All pages has been formatted.');
+  }
 }
